7da8a2af2bc9bdaaa98094b2cbaa13540c6d6a34
AndriyZin <unknown222.40@gmail.com>
2020-05-13 20:45:27 +0300

    simplify starter

--- webpack.config.js
+++ webpack.config.js
@@ -17,37 +17,17 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['.ts', '.js'],
-        modules: [
-            ROOT,
-            'node_modules'
-        ]
+        extensions: ['.ts', '.js']
     },
 
     module: {
         rules: [
             /****************
-            * PRE-LOADERS
-            *****************/
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                use: 'source-map-loader'
-            },
-            {
-                enforce: 'pre',
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: 'tslint-loader'
-            },
-
-            /****************
             * LOADERS
             *****************/
             {
                 test: /\.ts$/,
-                exclude: [ /node_modules/ ],
-                use: 'awesome-typescript-loader'
+                loader: 'ts-loader'
             }
         ]
     },
